fcefa79f69a0ec778253085fb43466ad7351e300
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-07-22 00:38:40 +0530

    section 6-user profiles

--- users/views.py
+++ users/views.py
@@ -5,3 +5,12 @@ def profiles(request):
     profiles=Profile.objects.all()
     context={'profiles':profiles}
     return render(request,'users/profile.html',context)
+
+def userProfile(request,pk):
+    profile=Profile.objects.get(id=pk)
+
+    topSkills=profile.skill_set.exclude(description="")
+    otherSkills=profile.skill_set.filter(description="")
+
+    context={'profile':profile,'topSkills':topSkills,'otherSkills':otherSkills}
+    return render(request,'users/user-profile.html',context)
