db2d42c762abab12df2dab7701d34a825304d7e2
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-07-29 00:33:18 +0530

    section 7 - part 2 - helper text styling

--- staticfiles/uikit/app.js
+++ staticfiles/uikit/app.js
@@ -2,3 +2,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   hljs.highlightAll();
 });
+
+// Select all alert close buttons
+let alertCloseButtons = document.querySelectorAll('.alert__close');
+
+// Add click event listener to each alert close button
+alertCloseButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    // Find the parent alert element and hide it when the close button is clicked
+    let alertWrapper = this.closest('.alert');
+    if (alertWrapper) {
+      alertWrapper.style.display = 'none';
+    }
+  });
+});
