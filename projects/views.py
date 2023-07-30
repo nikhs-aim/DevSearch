1da3fd35c12832467018779a6d496c1f52056342
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-07-30 18:32:02 +0530

    section 8 - search projects

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Project
+from .models import Project,Tag
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+
+
+
 
 def projects(request):
-    projects=Project.objects.all()
-    context={'projects':projects}
+
+    search_query=''
+
+    if request.GET.get('search_query'):
+        search_query=request.GET.get('search_query')
+
+    tags=Tag.objects.filter(name__icontains=search_query)
+
+    projects=Project.objects.distinct().filter(
+        Q(title__icontains=search_query)|
+        Q(description__icontains=search_query)|
+        Q(owner__name__icontains=search_query)|
+        Q(tags__in=tags)
+    )
+
+    context={'projects':projects,'search_query':search_query}
     return render(request,'projects/projects.html',context)
 
 
