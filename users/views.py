a9bf3a0e67b3c351ad4aafa109d575796008a514
Nikhitha R Karanth <nikhitharkaranth@gmail.com>
2023-07-30 18:22:06 +0530

    section 8 -using utils.py (profile search trigger)

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate,logout
-from .models import Profile,Skill
+from .models import Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import CustomUserCreationForm,ProfileForm,SkillForm
 from django.db.models import Q
+from .utils import searchProfiles
 
 def loginUser(request):
     page='login'
@@ -66,19 +67,7 @@ def registerUser(request):
 
 
 def profiles(request):
-    search_query=''
-
-    if request.GET.get('search_query'):
-        search_query=request.GET.get('search_query')
-    print('SEARCH:',search_query)
-
-    skills=Skill.objects.filter(name__icontains=search_query)
-    
-    profiles=Profile.objects.distinct().filter(
-        Q(name__icontains=search_query)|
-        Q(short_intro__icontains=search_query)|
-        Q(skill__in=skills)
-        )
+    profiles,search_query=searchProfiles(request)
     context={'profiles':profiles,'search_query':search_query}
     return render(request,'users/profile.html',context)
 
